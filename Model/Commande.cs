0e2f9a1b7c434bec0a187d15efeebaaf72795a5c
martin <martin.lenaerts@hotmail.fr>
2021-10-15 09:53:18 +0200

    initial commit

---
+++ Model/Commande.cs
@@ -0,0 +1,67 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Commande {
+
+	public Commande() {
+	}
+
+	private int id;
+
+	private time heure;
+
+	private Date date;
+
+	public void generateFacture() {
+		// TODO implement here
+	}
+
+	/// <summary>
+	/// @return
+	/// </summary>
+	public int getId() {
+		// TODO implement here
+		return 0;
+	}
+
+	/// <summary>
+	/// @param value
+	/// </summary>
+	public void setId(int value) {
+		// TODO implement here
+	}
+
+	/// <summary>
+	/// @return
+	/// </summary>
+	public time getHeure() {
+		// TODO implement here
+		return null;
+	}
+
+	/// <summary>
+	/// @param value
+	/// </summary>
+	public void setHeure(time value) {
+		// TODO implement here
+	}
+
+	/// <summary>
+	/// @return
+	/// </summary>
+	public Date getDate() {
+		// TODO implement here
+		return null;
+	}
+
+	/// <summary>
+	/// @param value
+	/// </summary>
+	public void setDate(Date value) {
+		// TODO implement here
+	}
+
+}
---
+++ Model/Pizza.cs
@@ -0,0 +1,18 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract class Pizza {
+
+	public Pizza() {
+	}
+
+
+
+	public void getPrix() {
+		// TODO implement here
+	}
+
+}
