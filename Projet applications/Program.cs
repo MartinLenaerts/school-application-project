94a642f04d79035f0efb2871ca93da6b30b56bec
Mael Mercier <mael.mercier@laposte.net>
2021-10-21 14:15:58 +0200

    Ajoutez des fichiers projet.

---
+++ Projet applications/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projet_applications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
